a1c80d2cc232374c1fbe0e4748d12acb60a8a2b7
juggernautjake <jacobmaddux96@gmail.com>
2023-09-07 17:55:19 -0500

    navbar now keeps track of which page you are on

--- navbar.js
+++ navbar.js
@@ -8,58 +8,82 @@ function NavBar(){
     return(
       <>
       <nav className="navbar navbar-expand-lg navbar-light bg-success">
-        <button className="btn-success btn shadow-none">
-          <a className="navbar-brand" href="#">BadBank</a>
-        </button>
+        <a className="nav-link navbar-text" href="#">
+          <button className="fw-bold btn-lg btn-success btn shadow-none navbar-text" focus type="button" id="deposit">
+            BadBank
+          </button>
+        </a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <button className="btn-success btn shadow-none">
-                <a className="nav-link" href="#/CreateAccount/">Create Account</a>
-              </button>
+              <a className="nav-link navbar-text" href="#">
+                <button className="btn-success btn shadow-none navbar-text" focus type="button" id="deposit">
+                  Home
+                </button>
+              </a>
             </li>
             <li className="nav-item">
-              <button className="btn-success btn shadow-none">
-                <a className="nav-link" href="#/login/">Login</a>
-              </button>
+              <a className="nav-link navbar-text" href="#/CreateAccount/">
+                <button className="btn-success btn shadow-none navbar-text" focus type="button" id="deposit">
+                  Create Account
+                </button>
+              </a>
             </li>
             <li className="nav-item">
-              <button className="btn-success btn shadow-none">
-                <a className="nav-link" href="#/deposit/">Deposit</a>
-              </button>
+              <a className="nav-link navbar-text" href="#/login/">
+                <button className="btn-success btn shadow-none navbar-text" focus type="button" id="deposit">
+                  Login
+                </button>
+              </a>
             </li>
             <li className="nav-item">
-              <button className="btn-success btn shadow-none">
-                <a className="nav-link" href="#/withdraw/">Withdraw</a>
-              </button>
+              <a className="nav-link navbar-text" href="#/deposit/">
+                <button className="btn-success btn shadow-none navbar-text" focus type="button" id="deposit">
+                  Deposit
+                </button>
+              </a>
             </li>
             <li className="nav-item">
-              <button className="btn-success btn shadow-none">
-                <a className="nav-link" href="#/balance/">Balance</a>
-              </button>
+              <a className="nav-link navbar-text" href="#/withdraw/">
+                <button className="btn-success btn shadow-none navbar-text" focus type="button" id="deposit">
+                  Withdraw
+                </button>
+              </a>
             </li>
             <li className="nav-item">
-              <button className="btn-success btn shadow-none">
-                <a className="nav-link" href="#/alldata/">AllData</a>
-              </button>
+              <a className="nav-link navbar-text" href="#/balance/">
+                <button className="btn-success btn shadow-none navbar-text" focus type="button" id="deposit">
+                  Balance
+                </button>
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link navbar-text" href="#/alldata/">
+                <button className="btn-success btn shadow-none navbar-text" focus type="button" id="deposit">
+                  AllData
+                </button>
+              </a>
             </li>          
           </ul>
           <ul className="navbar-nav ml-auto" >
             <li className="nav-item">
-            <button className="btn-success btn shadow-none" >
-                <a className="nav-link" href="#login">
+              <a className="nav-link navbar-text" href="#/profile/">
+                <button className="btn-success btn shadow-none navbar-text" focus type="button" id="deposit">
                   My Account
-                </a>
-              </button>
+                </button>
+              </a>
             </li>
           </ul>
         </div>
       </nav>
+
       </>
     );
   }
+
+
 
  
